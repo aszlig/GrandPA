40184618893de5bde3f239db2b95aab1374f8651
aszlig <aszlig@redmoonstudios.org>
2010-01-29 12:48:45 +0100

    Added inputdev dir and fixed path of _mouse.so.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 from distutils.core import setup, Extension
 
-mouse = Extension('grandpa._mouse',
+mouse = Extension('grandpa.inputdev._mouse',
                   libraries = ['gpm'],
                   sources = ['mouse.c'])
 
@@ -12,4 +12,5 @@ setup(name = 'GrandPA',
       author_email = 'aszlig@redmoonstudios.org',
       ext_modules = [mouse],
       scripts = ['bin/grandpa'],
-      packages=['grandpa', 'grandpa.interface', 'grandpa.chasers'])
+      packages=['grandpa', 'grandpa.interface', 'grandpa.inputdev',
+                'grandpa.chasers'])
