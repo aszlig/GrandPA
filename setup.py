64fe759b5edfece45d07d63009123859afe4e630
aszlig <aszlig@redmoonstudios.org>
2010-02-14 07:01:17 +0100

    Rewrote mouse/_mouse C modules as a cython module.

--- setup.py
+++ setup.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 from distutils.core import setup, Extension
+from Cython.Distutils import build_ext
 
-mouse = Extension('grandpa.inputdev._mouse',
+mouse = Extension('grandpa.inputdev.mouse',
                   libraries = ['gpm'],
-                  sources = ['ext/mouse.c'])
+                  sources = ['grandpa/inputdev/mouse.pyx'])
 
 fb = Extension('grandpa.interface._fb',
-                  sources = ['ext/fb.c'])
+               sources = ['ext/fb.c'])
 
 setup(name = 'GrandPA',
       version = '0.5',
       description = 'The GrandPA LedBar lighting controller',
       author = 'aszlig',
       author_email = 'aszlig@redmoonstudios.org',
+      cmdclass = {'build_ext': build_ext},
       ext_modules = [mouse, fb],
       scripts = ['bin/grandpa'],
-      packages=['grandpa', 'grandpa.interface', 'grandpa.inputdev',
-                'grandpa.chasers'])
+      packages = ['grandpa', 'grandpa.interface', 'grandpa.inputdev',
+                  'grandpa.chasers'])
